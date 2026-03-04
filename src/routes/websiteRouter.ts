1b6c55ce6ecd408d7d3ff4208fb4744dbc88837e
Vinay B S <vinayvinay0256@gmail.com>
2026-03-04 10:45:32 +0530

    Fix missing badge, PDF, CSV, public status routes

--- src/routes/websiteRouter.ts
+++ src/routes/websiteRouter.ts
@@ -14,9 +14,17 @@ import {
   clearMaintenance,
   updateWebsiteTags,
   getSecurityHeaders,
+  getUptimeBadge,
+  getPublicStatus,
+  downloadPdfReport,
+  exportCsv,
 } from '../controllers/websiteController';
 
 export const websiteRouter = express.Router();
+
+// Public routes
+websiteRouter.get('/badge/:id', getUptimeBadge);
+websiteRouter.get('/public/:id', getPublicStatus);
 
 websiteRouter.use(authMiddleware);
 
@@ -33,5 +41,7 @@ websiteRouter.put('/:id/maintenance', setMaintenance);
 websiteRouter.delete('/:id/maintenance', clearMaintenance);
 websiteRouter.put('/:id/tags', updateWebsiteTags);
 websiteRouter.get('/:id/security', getSecurityHeaders);
+websiteRouter.get('/:id/report/pdf', downloadPdfReport);
+websiteRouter.get('/:id/export/csv', exportCsv);
 
 export default websiteRouter;
